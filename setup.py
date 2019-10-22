942b25b9b734c017f9a08c7b8afd47675c5043bb
RGCTech-Dev <56837775+RGCTech-Dev@users.noreply.github.com>
2019-10-22 08:36:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author_email = 'aaron5671.outlook@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/RGCTech-Dev/MD5-Brute-Force',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  keywords = ['MD5', 'Brute Force', 'Cracker'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'hashlib',
       ],
